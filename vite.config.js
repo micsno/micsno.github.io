fdab930a76a2015521415ce811dac5aa8b4ff4e3
micsno <micsno@pm.me>
2024-08-02 12:02:46 +0300

    Update homepage, vite config, and 404.html for correct routing

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/micsno.github.io/', // Aseta tämä polku perus-URL:iksi
+  base: '/micsno.github.io/',
   plugins: [react()],
   build: {
     sourcemap: true
